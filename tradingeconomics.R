a6106f27e181b2f4013c651336f72476d940e28a
JoseBurg <joseaburgos01@outlook.com>
2025-11-26 20:28:04 +0000

    Conflicto de reporte resuelto.

--- tradingeconomics.R
+++ tradingeconomics.R
@@ -1,3 +1,16 @@
 # Funciones para obtener datos de trading economics
 
-devtools::install_github("tradingeconomics/tradingeconomics/R/tradingeconomics")
+# devtools::install_github("tradingeconomics/tradingeconomics/R/tradingeconomics")
+library(tradingeconomics)
+
+login("69a2c2a053a544e:uylny62mglro48t")
+
+
+
+getHistoricalData(country = 'united kingdom', indicator = 'imports')
+
+
+
+# Documentación -----------------------------------------------------------
+# https://github.com/tradingeconomics/tradingeconomics/tree/master/R
+
